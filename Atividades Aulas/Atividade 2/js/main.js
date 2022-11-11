f3ba00c0f7f68b01c07cfddb252fa11909338448
victup <victor.gsantos@hotmail.com>
2022-11-11 18:53:29 -0300

    Colocando Linha de total na tabela

--- Atividades Aulas/Atividade 2/js/main.js
+++ Atividades Aulas/Atividade 2/js/main.js
@@ -1,7 +1,7 @@
-let newTable = document.createElement("table");
-let header = newTable.createTHead();
-let tableBody = newTable.createTBody();
-let footer = newTable.createTFoot();
+const newTable = document.createElement("table");
+const header = newTable.createTHead();
+const tableBody = newTable.createTBody();
+const footer = newTable.createTFoot();
 
 document.body.appendChild(newTable);
 
@@ -18,13 +18,13 @@ const animalsContent = [
         "nome": "Tigre",
         "especie": "Tigre-de-bengala",
         "pais": "Bangladesh, Índia",
-        "exemplares": "< 2000"
+        "exemplares": 2000
       },
     {
         "nome": "Arara",
         "especie": "Ararinha-azul",
         "pais": "Brasil",
-        "exemplares": "< 60"
+        "exemplares": 60
       },
   
     {
@@ -52,16 +52,14 @@ const animalsContent = [
         "nome": "Papagaio",
         "especie": "Papagaio-mocho ou Kakapo",
         "pais": "Nova Zelândia",
-        "exemplares": "100"
+        "exemplares": 100
       }
 ]
-
 
 function addAnimalsTable(animalsContent) {
   animalsContent.forEach(animal => 
     mapObjectToCell(animal)
   );
-
 }
 
 function mapObjectToCell(animal) {
@@ -72,7 +70,6 @@ function mapObjectToCell(animal) {
     newRow.appendChild(newColumn);
     newColumn.textContent = item;
   });
-
 }
 
 function addTitlesTable(titles) {
@@ -89,9 +86,32 @@ function addTableStyle (){
   document.body.style.backgroundColor = "#9E1C44";
 
   header.style.backgroundColor = "rgba(255,255,255,0.3)";
+  footer.style.backgroundColor = "rgba(255,255,255,0.3)";
+
+  let tdFooter = footer.getElementsByTagName("td");
+
+  tdFooter[0].colSpan = "3";
+  tdFooter[0].backgroundColor = "rgba(255,255,255,0.3)";  
 
   tableBody.setAttribute("class", "tbl-content");
   newTable.setAttribute("class", "table");
+}
+
+function addValueToFooter(animalsContent){
+  const footerRow = footer.insertRow();
+  const footerTitle = footerRow.insertCell();
+  const footerCell = footerRow.insertCell();
+  footerTitle.textContent = "Total de exemplares:";
+  footerCell.textContent = getSumFromArray(animalsContent);   
+}
+
+function getSumFromArray(animalsContent){
+  let sum = 0;
+
+  animalsContent.forEach(item => {
+    sum += item.exemplares;
+  });
+  return sum;
 }
 
 function addCSSFile () {
@@ -106,5 +126,6 @@ function addCSSFile () {
 
 addTitlesTable(titles);
 addAnimalsTable(animalsContent);
+addValueToFooter(animalsContent);
 addCSSFile();
 addTableStyle();
